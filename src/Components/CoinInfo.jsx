09dc98035c502bcf1bdc5bdbe84f6aed3146ecea
rudyorahin <rudydinha@gmail.com>
2024-03-29 21:01:34 -0700

    first commit

---
+++ src/Components/CoinInfo.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+function CoinInfo ({ key, image, name, symbol }) {
+    const [price, setPrice] = useState(null);
+
+    useEffect(() => {
+
+        const getCoinPrice = async () => {
+            const response = await fetch(
+                `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` +
+                API_KEY
+            );
+
+            const json = await response.json();
+            setPrice(json);
+        }
+
+        getCoinPrice().catch(console.error);
+    }, [symbol]);
+
+    return (
+        <>
+            <div>
+                {price && (
+                    <li className="main-list" key={symbol}>
+                        <img
+                            className="icons"
+                            src={`https://www.cryptocompare.com${image}`}
+                            alt={`Small icon for ${name} crypto coin`}
+                            height="100px"
+                            width="100px"
+                        />
+                        <br></br>
+                        {name} <span className="tab"></span> ${price.USD} USD
+                    </li>
+                )}
+            </div>
+        </>
+    );
+    
+}
+
+export default CoinInfo
